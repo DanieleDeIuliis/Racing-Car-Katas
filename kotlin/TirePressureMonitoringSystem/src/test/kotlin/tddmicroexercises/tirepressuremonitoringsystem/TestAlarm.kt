c934f70a2ecb7b57eaf0a1e8c010451d00c6fe38
Pietro Di Bello <pierodibello@gmail.com>
2020-06-30 15:57:35 +0200

    mob next [ci-skip]

--- kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
+++ kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
@@ -2,12 +2,20 @@ package tddmicroexercises.tirepressuremonitoringsystem
 
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Ignore
 
 class TestAlarm {
 
     @Test
-    fun foo() {
+    fun `initially the alarm is always off`() {
         val alarm = Alarm()
         assertEquals(false, alarm.isAlarmOn)
+    }
+
+    @Test @Ignore
+    fun `raise the alarm when the pressure measured by the sensor is under the lower threshold`() {
+        val alarm = Alarm()
+        alarm.check()
+        assertTrue(alarm.isAlarmOn)
     }
 }
