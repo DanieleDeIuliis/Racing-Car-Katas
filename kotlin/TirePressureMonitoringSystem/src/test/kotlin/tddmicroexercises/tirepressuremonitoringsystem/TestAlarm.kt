4ee7ddb1fffa2d64f4d3e065050ab9f8a0dfc52e
Pietro Di Bello <pierodibello@gmail.com>
2020-07-02 10:38:21 +0200

    refactor tests

--- kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
+++ kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
@@ -13,28 +13,26 @@ class TestAlarm {
 
     @Test
     fun `one check should set alarm to on with high pressure`() {
-        val alarm = Alarm(sensor = object : Sensor {
-            override fun popNextPressurePsiValue(): Double = 21.1
-        })
+        val alarm = Alarm(sensor = ProgrammableSensor(21.1))
         alarm.check()
         assertEquals(true, alarm.isAlarmOn)
     }
 
     @Test
     fun `one check should set alarm to on with low pressure`() {
-        val alarm = Alarm(sensor = object : Sensor {
-            override fun popNextPressurePsiValue(): Double = 0.0
-        })
+        val alarm = Alarm(sensor = ProgrammableSensor(0.0))
         alarm.check()
         assertEquals(true, alarm.isAlarmOn)
     }
 
     @Test
     fun `alarm is off if the pressure is within specification`() {
-        val alarm = Alarm(sensor = object : Sensor {
-            override fun popNextPressurePsiValue(): Double = 20.0
-        })
+        val alarm = Alarm(sensor = ProgrammableSensor(20.0))
         alarm.check()
         assertEquals(false, alarm.isAlarmOn)
+    }
+
+    class ProgrammableSensor(private val pressure: Double) : Sensor {
+        override fun popNextPressurePsiValue(): Double = pressure
     }
 }
