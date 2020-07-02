d5dab0aeb0df408757284221e272eab658c22cac
Alessandro Piccioli <ap@zextras.com>
2020-07-02 10:43:35 +0200

    mob next [ci-skip]

--- kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
+++ kotlin/TirePressureMonitoringSystem/src/test/kotlin/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.kt
@@ -12,33 +12,6 @@ class TestAlarm {
     }
 
     @Test
-    fun `Alarm is on when pressure is under lower limit`() {
-        val alarm = Alarm()
-
-        alarm.check(16.9999999)
-
-        assertEquals(true, alarm.isAlarmOn)
-    }
-
-    @Test
-    fun `Alarm is on when pressure is over upper limit`() {
-        val alarm = Alarm()
-
-        alarm.check(21.0000001)
-
-        assertEquals(true, alarm.isAlarmOn)
-    }
-
-    @Test
-    fun `Alarm is off when pressure is between limits`() {
-        val alarm = Alarm()
-
-        alarm.check(20.0)
-
-        assertEquals(false, alarm.isAlarmOn)
-    }
-
-    @Test
     fun `Alarm is on when pressure is under lower limit with TestAlarm`() {
         val alarm = TestableAlarm(16.9999)
 
@@ -48,4 +21,21 @@ class TestAlarm {
     }
 
 
+    @Test
+    fun `Alarm is on when pressure is over upper limit with TestAlarm`() {
+        val alarm = TestableAlarm(21.1)
+
+        alarm.check()
+
+        assertEquals(true, alarm.isAlarmOn)
+    }
+
+    @Test
+    fun `Alarm is off when pressure is between limits with TestAlarm`() {
+        val alarm = TestableAlarm(20.0)
+
+        alarm.check()
+
+        assertEquals(false, alarm.isAlarmOn)
+    }
 }
