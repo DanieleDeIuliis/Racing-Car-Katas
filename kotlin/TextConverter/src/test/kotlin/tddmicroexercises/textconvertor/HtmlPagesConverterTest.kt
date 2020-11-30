961e8897c28e13d1465dbd54f01f1739d8c177a9
DLU <daniele.deiuliis@gmail.com>
2020-11-30 17:02:49 +0100

    added single and multi page tests

--- kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverterTest.kt
+++ kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlPagesConverterTest.kt
@@ -8,8 +8,17 @@ import org.junit.Assert.*
 class HtmlPagesConverterTest {
     @Test
     @Throws(IOException::class)
-    fun foo() {
-        val converter = HtmlPagesConverter("foo")
-        assertEquals("fixme", converter.filename)
+    fun `return a single page`() {
+        val pathName = this::class.java.getResource("/testSinglePage").path
+        val converter = HtmlPagesConverter(pathName)
+        assertEquals("Ciao sono un file di test.<br />Vediamo se funziona il multilinea.<br />", converter.getHtmlPage(0))
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun `return the second page`() {
+        val pathName = this::class.java.getResource("/testMultiPage").path
+        val converter = HtmlPagesConverter(pathName)
+        assertEquals("Vediamo se funziona il multilinea.<br />", converter.getHtmlPage(1))
     }
 }
