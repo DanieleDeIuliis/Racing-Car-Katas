37afebdabde8bf757a68aaaa3f40f3ec0633709d
DLU <daniele.deiuliis@gmail.com>
2020-11-30 16:23:21 +0100

    added tests to HtmlTextConverter

--- kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlTextConverterTest.kt
+++ kotlin/TextConverter/src/test/kotlin/tddmicroexercises/textconvertor/HtmlTextConverterTest.kt
@@ -5,8 +5,44 @@ import org.junit.Test
 
 class HtmlTextConverterTest {
     @Test
-    fun foo() {
-        val converter = HtmlTextConverter("foo")
-        assertEquals("fixme", converter.filename)
+    fun `return the lines with html new line value at the end of each line `() {
+        val pathName = this::class.java.getResource("/test").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Ciao sono un file di test.<br />Vediamo se funziona il multilinea.<br />", converter.convertToHtml())
+    }
+
+    @Test
+    fun `return a line with the html new line at the end and  & symbol converted `() {
+        val pathName = this::class.java.getResource("/test&").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Vediamo se funziona &amp;<br />", converter.convertToHtml())
+    }
+
+    @Test
+    fun `return a line with the html new line at the end and  minus symbol converted `() {
+        val pathName = this::class.java.getResource("/testMinus").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Vediamo se funziona &lt;<br />", converter.convertToHtml())
+    }
+
+    @Test
+    fun `return a line with the html new line at the end and  greater symbol converted `() {
+        val pathName = this::class.java.getResource("/testGreater").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Vediamo se funziona &gt;<br />", converter.convertToHtml())
+    }
+
+    @Test
+    fun `return a line with the html new line at the end and ' symbol converted `() {
+        val pathName = this::class.java.getResource("/testSingleQuot").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Vediamo se funziona &quot;<br />", converter.convertToHtml())
+    }
+
+    @Test
+    fun `return a line with the html new line at the end and " symbol converted `() {
+        val pathName = this::class.java.getResource("/testDoubleQuot").path
+        val converter = HtmlTextConverter(pathName)
+        assertEquals("Vediamo se funziona &quot;<br />", converter.convertToHtml())
     }
 }
